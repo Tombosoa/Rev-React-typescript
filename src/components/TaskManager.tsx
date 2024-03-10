67d67f10ef8a715d4e9e91e8a00d9e66d705c473
Tombosoa <hei.nantenaina.4@gmail.com>
2024-03-10 19:45:31 +0300

    feat: use useTaskManager on TaskManager

--- src/components/TaskManager.tsx
+++ src/components/TaskManager.tsx
@@ -1,55 +1,22 @@
 import { nanoid } from "nanoid";
 import { SetStateAction, useState } from "react";
 import "./TaskManager.css";
+import UseTaskManager from "./useTaskManager";
 
 // TODO: create custom hook to manage task state
 export const TaskManager = () => {
   const [title, setTitle] = useState("");
   const [searchKeyword, setSearchKeyword] = useState("");
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const { tasks,completeTask, updateTask, addTask, filteredTasks } = UseTaskManager();
+  const handleSearch=(e:{target:{value:string}})=>{
+   setSearchKeyword(e.target.value)
+  }
+  const handleAddTask=()=>{
+    addTask(title)
+    setTitle("")
 
-  
-
-  // remove task from list
-  const completeTask = (id:string) => {
-    setTasks(tasks.filter((task: { id: string }) => task.id !== id));
-    
-  };
-
-  const updateTask = (id:string, taskUpdate:string) => {
-    const newTasks = tasks.slice();
-
-    const index = tasks.findIndex((task:{id:string}) => task.id === id);
-
-    newTasks[index].title = taskUpdate 
-
-    setTasks(newTasks);
-  };
-
-  const addTask = () => {
-    if (title.length < 1) {
-      return;
-    }
-
-    const newTask:Task= {
-      // using nanoid to generate unique id
-      id: nanoid(),
-      title,
-    };
-   
-    
-    setTasks((prevTasks)=>prevTasks.concat(newTask))
-    setTitle("");
-  };
-
-  const handleSearch = (ev: { target: { value: SetStateAction<string>; }; }) => {
-    setSearchKeyword(ev.target.value);
-  };
-
-  const filteredTasks = tasks.filter((task:{title:string}) =>
-    task.title.toLowerCase().includes(searchKeyword.toLowerCase()),
-  );
-
+  }
+  const tasksFilterd = filteredTasks(searchKeyword)
   return (
     <div className="container">
       <h1>Task Manager</h1>
@@ -67,11 +34,11 @@ export const TaskManager = () => {
           }}
         />
 
-        <button onClick={addTask}>Add Task</button>
+        <button onClick={handleAddTask}>Add Task</button>
       </div>
 
       <ul className="container">
-        {filteredTasks.map((task) => (
+        {tasksFilterd.map((task) => (
           <li key={task.id} className="task">
             <div className="task">
               <input
